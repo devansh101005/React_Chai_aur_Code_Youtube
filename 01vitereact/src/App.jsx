d1ea78e5104d9a37cf8878894c923939b279fe20
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-09-20 20:15:56 +0530

    understtod basic jsx convention

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,14 @@
 
-
+import Chai from "./chai"
 function App() {
 
 
   return (
-    <h1>Chai aur react with vite|Devansh</h1>
+    // <h1>Chai aur react with vite|Devansh</h1>
+    <Chai/>
+    //yahan ek baar mai ek hi element export kar skte hain
+    //ye jsx ka rule hai
+    // to escape this   div bna do 
   )
 }
 
